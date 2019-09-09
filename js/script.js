ec6e6b36c473ab4537cf6e15b8b934eabd6b1795
Konrad Lis <o.konradlis@gmail.com>
2019-09-09 15:59:02 +0200

    Add js code

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var computerMove, playerMove, item;
+playerMove = 'papier';
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz !');
