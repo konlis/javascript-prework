1bb1fe3d0338c121c2526f7177ecbfe290683322
Konrad Lis <o.konradlis@gmail.com>
2019-09-09 20:14:25 +0200

    Repare script.js code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
 if (playerInput == '1') {
   playerMove = 'kamień';
 } else if (playerInput == '2') {
@@ -10,5 +25,5 @@ if (playerInput == '1') {
 } else {
   playerMove = 'kamień';
 }
-printMessage('Wykonałeś domyślny ruch, rzuć jeszcze raz.');
+printMessage('Wykonałeś domyślny ruch, losuj jeszcze raz.');
 printMessage('Twój ruch: ' + playerMove);
